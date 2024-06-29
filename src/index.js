61f5f214c3ed7efeebd2519d1ffeb5fda0362df8
Faizan <faizanamjad575@gmail.com>
2024-06-29 18:46:21 +0500

    Create a default project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './style.css';
+import {Task, createProject} from './taskConstructor';
 
 var projects = []; //to store all projects
+
+createProject("Default", projects); //the default project
