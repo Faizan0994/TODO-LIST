8c28d62da0d2a0a72be2b3fef80e0c484d6467b8
Faizan <faizanamjad575@gmail.com>
2024-06-29 18:56:05 +0500

    Create a function to create tasks

--- src/taskConstructor.js
+++ src/taskConstructor.js
@@ -7,8 +7,12 @@ class Task {
     }
 }
 
+function createTask(project, title, description, date, priority) {
+    project.push(new Task(title, description, date, priority));
+}
+
 function createProject(name, collection){
     collection.push(new Array(name));
 }
 
-export {Task, createProject};
+export {Task, createProject, createTask};
