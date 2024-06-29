449c183565424c790e7d3a22e4ed6b4c5d7d4f28
Faizan <faizanamjad575@gmail.com>
2024-06-29 19:21:46 +0500

    display the default project on the page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.css';
 import {createProject, createTask} from './taskConstructor';
+import {displayProjects} from './DOMhandler';
 
 var projects = []; //to store all projects
 
 createProject("Default", projects); //the default project
+
+const projectsTab = document.querySelector('.projects');
+displayProjects(projectsTab, projects);
