5f11dcca1f425fc687301c541860d1c655f655b5
Faizan <faizanamjad575@gmail.com>
2024-06-30 12:10:04 +0500

    Add some dummy tasks and projects

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import {displayProjects} from './DOMhandler';
 var projects = []; //to store all projects
 
 createProject("Default", projects); //the default project
+createProject("number1", projects);
+createProject("number2", projects);
+createTask(projects[0], "Do something already planned", "This is a very simple task for demonstration", "30/6/2024", "important");
+createTask(projects[0], "Do something again", "This is a very simple task for demonstration", "30/6/2024", "important");
+createTask(projects[1], "Do something new", "This is a very simple task for demonstration", "30/6/2024", "important");
+createTask(projects[1], "Repeat the previous task", "This is a very simple task for demonstration", "30/6/2024", "important");
+createTask(projects[2], "A relatively new task", "This is a very simple task for demonstration", "30/6/2024", "important");
+createTask(projects[2], "This is another task", "This is a very simple task for demonstration", "30/6/2024", "important");
 
 const projectsTab = document.querySelector('.projects');
 displayProjects(projectsTab, projects);
