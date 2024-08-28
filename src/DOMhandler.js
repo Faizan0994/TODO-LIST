7de8513be206acb9d17a0fb3d640628e7c82c1ef
Faizan <faizanamjad575@gmail.com>
2024-08-28 16:32:08 +0500

    Create function to bring up form for new tasks

--- src/DOMhandler.js
+++ src/DOMhandler.js
@@ -1,4 +1,5 @@
 import { deleteTask } from "./taskConstructor";
+import { takeFormInput } from "./index";
 
 function displayProjects(section, projects) {
     while(section.firstElementChild){
@@ -83,4 +84,95 @@ function displayExtendedTask(taskElement, collection) {
     }
 }
 
-export {displayProjects, displayTasks, displayExtendedTask};
+function displayNewTaskForm(home){
+    while(home.firstElementChild){
+        home.removeChild(home.lastElementChild);
+    }
+
+    //creates the form and all inputs
+    const form = document.createElement('form');
+    form.action = "";
+    
+    const legend = document.createElement('legend');
+    legend.textContent = "New Task";
+    form.appendChild(legend);
+
+    const row1 = document.createElement('div');
+    row1.classList.add('form-row');
+    const nameLabel = document.createElement('label');
+    nameLabel.textContent = "Name:";
+    nameLabel.setAttribute('for', 'taskName');
+    row1.appendChild(nameLabel);
+    const nameInput = document.createElement('input');
+    nameInput.setAttribute('type', 'text');
+    nameInput.setAttribute('id', 'taskName');
+    nameInput.setAttribute('name', 'taskName');
+    row1.appendChild(nameInput);
+    form.appendChild(row1);
+
+    const row2 = document.createElement('div');
+    row2.classList.add('form-row');
+    const descLabel = document.createElement('label');
+    descLabel.textContent = "Description:";
+    descLabel.setAttribute('for', 'description');
+    row2.appendChild(descLabel);
+    const descInput = document.createElement('input');
+    descInput.setAttribute('type', 'text');
+    descInput.setAttribute('id', 'description');
+    descInput.setAttribute('name', 'description');
+    row2.appendChild(descInput);
+    form.appendChild(row2);
+
+    const row3 = document.createElement('div');
+    row3.classList.add('form-row');
+    const dateLabel = document.createElement('label');
+    dateLabel.textContent = "Due Date:";
+    dateLabel.setAttribute('for', 'date');
+    row3.appendChild(dateLabel);
+    const dateInput = document.createElement('input');
+    dateInput.setAttribute('type', 'date');
+    dateInput.setAttribute('id', 'date');
+    dateInput.setAttribute('name', 'date');
+    row3.appendChild(dateInput);
+    form.appendChild(row3);
+
+    const row4 = document.createElement('div');
+    row4.classList.add('form-row');
+    const priorityLabel = document.createElement('label');
+    priorityLabel.textContent = "Priority:";
+    priorityLabel.setAttribute('for', 'priority');
+    row4.appendChild(priorityLabel);
+    const priorityInput = document.createElement('select');
+    priorityInput.setAttribute('id', 'priority');
+    priorityInput.setAttribute('name', 'priority');
+    const normal = document.createElement('option');
+    normal.setAttribute('value', 'normal');
+    normal.textContent = "normal";
+    priorityInput.appendChild(normal);
+    const important = document.createElement('option');
+    important.setAttribute('value', 'important');
+    important.textContent = "important";
+    priorityInput.appendChild(important);
+    const optional = document.createElement('option');
+    optional.setAttribute('value', 'optional');
+    optional.textContent = "optional";
+    priorityInput.appendChild(optional);
+    row4.appendChild(priorityInput);
+    form.appendChild(row4);
+
+    const addTaskButton = document.createElement('button');
+    addTaskButton.setAttribute('type', 'submit');
+    addTaskButton.classList.add('form-add-task');
+    addTaskButton.textContent = "Add Task";
+    form.appendChild(addTaskButton);
+
+    home.appendChild(form);
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const projectId = document.querySelector('.selected').id;
+        takeFormInput(nameInput.value, descInput.value, dateInput.value, priorityInput.value, projectId);
+    });
+}
+
+export {displayProjects, displayTasks, displayExtendedTask, displayNewTaskForm};
