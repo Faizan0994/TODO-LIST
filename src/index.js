f786838936cc0b164f37765431c0fe4298d5e709
Faizan <faizanamjad575@gmail.com>
2024-08-30 11:35:37 +0500

    Remove extra default tasks and projects

--- src/index.js
+++ src/index.js
@@ -7,14 +7,7 @@ if(localStorage.getItem("projectsArray") === null) {
     var projects = []; //to store all projects
 
     createProject("Default", projects); //the default project
-    createProject("number1", projects);
-    createProject("number2", projects);
     createTask(projects[0], "Do something already planned", "This is a very simple task for demonstration", "30/6/2024", "important");
-    createTask(projects[0], "Do something again", "This is a very simple task for demonstration", "30/6/2024", "important");
-    createTask(projects[1], "Do something new", "This is a very simple task for demonstration", "30/6/2024", "important");
-    createTask(projects[1], "Repeat the previous task", "This is a very simple task for demonstration", "30/6/2024", "important");
-    createTask(projects[2], "A relatively new task", "This is a very simple task for demonstration", "30/6/2024", "important");
-    createTask(projects[2], "This is another task", "This is a very simple task for demonstration", "30/6/2024", "important");
     console.log("default tasks created");
 } else {
     var projects = JSON.parse(localStorage.getItem("projectsArray"));
